ba6e3711a9f3142a65f2259da56f974ee082f269
Liam Lage <44103555+LiamLage@users.noreply.github.com>
2021-11-15 17:48:06 +0200

    Add projects

--- Section_03/_3-05_capturing_input/main/main.c
+++ Section_03/_3-05_capturing_input/main/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-void app_main(void)
-{
+void app_main(void) {
+    char c = 0;
+    char str[100];
+    memset(str, 0, sizeof(str));
 
+    printf("Enter some characters:\n");
+    while (c != '\n') {
+        c = getchar();
+        if (c != 0xFF) {
+            str[strlen(str)] = c;
+            printf("%c", c);
+        }
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+    printf("\n\nuser input:\n%s\n", str);
 }
